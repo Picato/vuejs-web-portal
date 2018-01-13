b6353d87da9b8aaee0b4f7a5efe440453f9f3d8e
thanhdt-viosoft <thanhdt@viosoft.com>
2018-01-13 12:42:53 +0700

    Update UI

--- components/_Market.vue
+++ components/_Market.vue
@@ -56,30 +56,30 @@
       <hr/>
       <table class="table is-bordered is-striped is-narrow is-fullwidth">
         <tr>
-          <th>Market-Coin</th>
-          <th @click="sortLast=sortBid=sortAsk=sortPercent=sortPrev=0; ++sortVolume;" class="is-cursor">Volume
+          <th width="120">Coin <span class="tag">Market</span></th>
+          <th @click="sortLast=sortBid=sortAsk=sortPercent=sortPrev=0; ++sortVolume;" class="is-cursor" width="350">Volume
             <span class="icon is-pulled-right">
               <i class="fa" v-show="this.sortVolume % 5 !== 0" :class="{'fa-level-up': [2, 4].includes(this.sortVolume % 5), 'fa-level-down': [1, 3].includes(this.sortVolume % 5)}"></i>&nbsp;
               <span v-show="[1, 2].includes(this.sortVolume % 5)"></span>
               <span v-show="[3, 4].includes(this.sortVolume % 5)">%</span>
             </span>
           </th>
-          <th @click="sortLast=sortVolume=sortAsk=sortPercent=sortBid=0; ++sortPrev;" class="is-cursor">Prev price
-            <span class="icon is-pulled-right">
-              <i class="fa" v-show="this.sortPrev % 3 !== 0" :class="{'fa-level-up': this.sortPrev % 3 === 2, 'fa-level-down': this.sortPrev % 3 === 1}"></i>
-            </span>
-          </th>
-          <th @click="sortVolume=sortBid=sortAsk=sortPercent=sortPrev=0; ++sortLast;" class="is-cursor">Last price (High - Low)
+          <th @click="sortVolume=sortBid=sortAsk=sortPercent=sortPrev=0; ++sortLast;" class="is-cursor" width="320">Last price (High - Low)
             <span class="icon is-pulled-right">
               <i class="fa" v-show="this.sortLast % 3 !== 0" :class="{'fa-level-up': this.sortLast % 3 === 2, 'fa-level-down': this.sortLast % 3 === 1}"></i>
             </span>
           </th>
-          <th @click="sortLast=sortVolume=sortAsk=sortPercent=sortPrev=0; ++sortBid;" class="is-cursor" v-if="ftime === 'RawHandler'">Bid price
+          <th @click="sortLast=sortVolume=sortAsk=sortPercent=sortBid=0; ++sortPrev;" class="is-cursor" width="180">Prev price
+            <span class="icon is-pulled-right">
+              <i class="fa" v-show="this.sortPrev % 3 !== 0" :class="{'fa-level-up': this.sortPrev % 3 === 2, 'fa-level-down': this.sortPrev % 3 === 1}"></i>
+            </span>
+          </th>          
+          <th @click="sortLast=sortVolume=sortAsk=sortPercent=sortPrev=0; ++sortBid;" class="is-cursor" width="180" v-if="ftime === 'RawHandler'">Bid price
             <span class="icon is-pulled-right">
               <i class="fa" v-show="this.sortBid % 3 !== 0" :class="{'fa-level-up': this.sortBid % 3 === 2, 'fa-level-down': this.sortBid % 3 === 1}"></i>
             </span>
           </th>
-          <th @click="sortLast=sortBid=sortVolume=sortPercent=sortPrev=0; ++sortAsk;" class="is-cursor" v-if="ftime === 'RawHandler'">Ask price
+          <th @click="sortLast=sortBid=sortVolume=sortPercent=sortPrev=0; ++sortAsk;" class="is-cursor" width="180" v-if="ftime === 'RawHandler'">Ask price
             <span class="icon is-pulled-right">
               <i class="fa" v-show="this.sortAsk % 3 !== 0" :class="{'fa-level-up': this.sortAsk % 3 === 2, 'fa-level-down': this.sortAsk % 3 === 1}"></i>
             </span>
@@ -92,18 +92,18 @@
         </tr>
         <tr v-for="(vl, k) in data" :key="k">
           <th>
-            <a :href="'https://bittrex.com/Market/Index?MarketName=' + vl.market + '-' + vl.name" :target="vl.name">{{vl.name}}</a><span class="tag">{{vl.market}}</span>            
+            <span class="tag">{{vl.market}}</span><a :href="'/coin/market/' + vl.market + '-' + vl.name" :target="vl.name">{{vl.name}}</a>
           </th>
           <td>
             <div class="tag"><b>{{vl.baseVolume | $number}}</b><span :class="'is-' + vl.market">&nbsp;{{vl.market}}</span></div>
             <div class="tag is-pulled-right" :class="{'has-text-danger': vl.baseVolumePercent < 0, 'has-text-success': vl.baseVolumePercent > 0}" v-if="vl.baseVolumePercent">{{vl.baseVolumePercent | $number}}%</div>
           </td>
           <td>          
-            <div class="tag"><b>{{vl.prev | $number}}</b>&nbsp;<i :class="'is-' + vl.market">{{vl.market}}</i></div>
-          </td>
-          <td>          
             <div class="tag"><b>{{vl.last | $number}}</b>&nbsp;<i :class="'is-' + vl.market">{{vl.market}}</i></div>
             <div class="tag" v-if="vl.low !== undefined && vl.high !== undefined">( {{vl.low | $number}} - {{vl.high | $number}} )</div>          
+          </td>
+          <td>          
+            <div class="tag"><b>{{vl.prev | $number}}</b>&nbsp;<i :class="'is-' + vl.market">{{vl.market}}</i></div>
           </td>
           <td v-if="ftime === 'RawHandler'">          
             <div class="tag"><b>{{vl.bid | $number}}</b>&nbsp;<i :class="'is-' + vl.market">{{vl.market}}</i></div>
